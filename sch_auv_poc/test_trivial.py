f5268eee258c264824e59588f1577ee086eee116
Dima Spivak <dima@streamsets.com>
2020-04-07 23:36:34 -0500

    Add test_trivial.py

---
+++ sch_auv_poc/test_trivial.py
@@ -0,0 +1,19 @@
+"""Tests for the "Trivial" pipeline."""
+
+import json
+
+
+def test_complete(sch):
+    """A test of the complete functioning of the pipeline."""
+    pipeline = sch.pipelines.get(name='Trivial')
+
+    SAMPLE_DATA = dict(company='StreamSets', valuation='$1,000,000,000')
+    runtime_parameters = dict(RAW_DATA=json.dumps(SAMPLE_DATA))
+
+    with sch.run_test_job(pipeline, runtime_parameters, data_collector_labels=['test']) as job:
+        job_data_collector = job.data_collectors[0]
+        data_collector = job_data_collector.instance
+        pipeline = job_data_collector.pipeline
+
+        snapshot = data_collector.capture_snapshot(pipeline).snapshot
+        assert [record.field for record in snapshot[pipeline.origin_stage].output] == [SAMPLE_DATA]
