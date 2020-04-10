c786ea50e59b03107be8d9331e174fc6e9242b68
Dima Spivak <dima@streamsets.com>
2020-04-10 01:17:12 -0500

    Add MVP of test result-aware test job handling

--- conftest.py
+++ conftest.py
@@ -4,8 +4,19 @@ import pytest
 def pytest_addoption(parser):
     parser.addoption('--data-collector-label', action='append')
 
-@pytest.fixture(scope='session')
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    result = outcome.get_result()
+    setattr(item, 'result', result)
+
+
+@pytest.fixture
 def sch(sch_session, request):
     data_collector_labels = request.config.getoption('data_collector_label') or []
     sch_session.data_collector_labels = data_collector_labels
     yield sch_session
+    if request.node.result.passed:
+        sch_session.delete_job(sch_session._test_job_queue.pop())
